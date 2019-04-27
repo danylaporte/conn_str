2e82f67d8891fe13aa6aa0f0df8979496c99700c
Dany Laporte <dany_laporte@hotmail.com>
2019-04-27 08:21:13 -0400

    improve documentation

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,38 @@
+//! Database connection string parsing library for Rust.
+//! 
+//! This crate contains method to parse and extract part of a connection string in several formats.
+//! There is also the possibility to encode a connection string using the `append_key_value` function.
+//! 
+//! # Supported formats
+//! 
+//! - Entity Framework (from the .net framework)
+//! - MS SQL (from the .net framework System.Data.SqlClient)
+//! 
+//! # Example
+//! 
+//! ```
+//! use conn_str::{append_key_value, MsSqlConnStr};
+//! use std::str::FromStr;
+//! 
+//! fn main() {
+//!     let conn = "data source=.\\SQL2017;initial catalog=Db1;";
+//!     let conn = MsSqlConnStr::from_str(conn).unwrap();
+//! 
+//!     assert_eq!(".\\SQL2017", conn.data_source().unwrap());
+//!     assert_eq!("Db1", conn.initial_catalog().unwrap());
+//! 
+//!     let mut new_conn = String::new();
+//! 
+//!     append_key_value(&mut new_conn, "data source", conn.data_source().unwrap(), false);
+//!     append_key_value(&mut new_conn, "initial catalog", conn.initial_catalog().unwrap(), false);
+//!     
+//!     // add a user and a password to the connection string
+//!     append_key_value(&mut new_conn, "user id", "john", false);
+//!     append_key_value(&mut new_conn, "password", "Pass1=3", false);
+//! 
+//!     assert_eq!(&new_conn, r#"data source=.\SQL2017;initial catalog=Db1;user id=john;password="Pass1=3""#);
+//! }
+//! ```
 use std::collections::HashMap;
 use std::error;
 use std::fmt;
@@ -54,12 +89,14 @@ impl EFConnStr {
 /// use conn_str::MsSqlConnStr;
 /// use std::str::FromStr;
 ///
-/// let b = MsSqlConnStr::from_str("data source=.\\Sql2017;Initial Catalog=Db1;integrated security=sspi;pwd='Test=1'").unwrap();
+/// let conn = "data source=.\\Sql2017;Initial Catalog=Db1;integrated security=sspi;pwd='Test=1'";
+/// let conn = MsSqlConnStr::from_str(conn).unwrap();
 ///
-/// assert_eq!(".\\Sql2017", b.data_source().unwrap());
-/// assert_eq!("Db1", b.initial_catalog().unwrap());
-/// assert_eq!(true, b.integrated_security().unwrap());
-/// assert_eq!("Test=1", b.password().unwrap());
+/// // gets the data_source
+/// assert_eq!(".\\Sql2017", conn.data_source().unwrap());
+/// 
+/// // gets the initial catalog
+/// assert_eq!("Db1", conn.initial_catalog().unwrap());
 /// ```
 pub struct MsSqlConnStr(HashMap<String, String>);
 
